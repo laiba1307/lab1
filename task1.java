b168146166044b66589a96e8dc9a75dfec227146
laiba1307 <114337724+laiba1307@users.noreply.github.com>
2024-10-17 15:24:29 +0500

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,23 @@
+public class task1 {
+
+	public static <E> void printArray(E[] inputArray) {
+		for(int element=inputArray.length-1;element>=0;element--) {
+			System.out.println(inputArray[element]);
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+		Integer[] intArray = { 1, 2, 3, 4, 6 };
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+		Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+		
+		System.out.println("integerArray :");
+		printArray(intArray);
+		System.out.println("doubleArray :");
+		printArray(doubleArray);
+		System.out.println("characterArray :");
+		printArray(charArray); 
+
+	}
+}
